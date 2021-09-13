ce3d2451dce0f30010ce1a21854be676ff8611e9
kim-es <eunsoo8606@naver.com>
2021-09-13 16:30:52 +0900

    무중단 배포 서비스 개선

--- src/config/ecosystem.config.js
+++ src/config/ecosystem.config.js
@@ -2,7 +2,9 @@ module.exports = {
     apps: [{
     name: 'web-server',
     script: '../main.js',
-    instances: 4,
-    exec_mode: `cluster`
+    instances: 0,
+    exec_mode: `cluster`,
+    wait_ready: true,
+    listen_timeout: 50000
     }]
   }
